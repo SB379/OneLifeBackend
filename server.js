34dc2aa48765681bee6f5711f282b0443a7f17d1
SB379 <sidbalaji03@gmail.com>
2023-01-27 20:43:36 -0500

    commit at 8:43 pm

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import experiences from './api/experiences.route.js'
-import user from './api/user.js';
+//import user from './api/user.js';
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/experiences", experiences)
-app.use("/api/user", user)
+//app.use("/api/user", user)
 
 
 //if anything else is typed, 404 not found error
